a1799c658b2931a0c4b06e42d88b20df383305bb
Gastón Tarasconi <gaston.tarasconi@gmail.com>
2024-02-25 16:36:39 -0300

    change api response to json format, create generic error function

--- src/server.rs
+++ src/server.rs
@@ -4,18 +4,37 @@ use std::sync::{Arc, Mutex};
 
 use super::model::*;
 
+use serde_json::json;
 use tiny_http::{Server, Request, Response, Header, Method, StatusCode};
 
+fn serve_error(request: Request, message: &str, status_code: StatusCode, log: bool, error: &str) -> io::Result<()> {
+    let content_type_header = Header::from_bytes("Content-Type", "application/json")
+        .expect("That we didn't put any garbage in the headers");
+
+    if log {
+        eprintln!("ERROR: {error}");
+    }
+
+    let error = json!({
+        "message": message,
+    });
+
+    request.respond(Response::from_string(serde_json::to_string(&error).unwrap())
+        .with_status_code(status_code)
+        .with_header(content_type_header)
+    )
+}
+
 fn serve_404(request: Request) -> io::Result<()> {
-    request.respond(Response::from_string("404").with_status_code(StatusCode(404)))
+    serve_error(request, "ERROR: Route not found", StatusCode(404), false, "")
 }
 
-fn serve_500(request: Request) -> io::Result<()> {
-    request.respond(Response::from_string("500").with_status_code(StatusCode(500)))
+fn serve_500(request: Request, message: &str, error: &str) -> io::Result<()> {
+    serve_error(request, message, StatusCode(500), true, error)
 }
 
-fn serve_400(request: Request, message: &str) -> io::Result<()> {
-    request.respond(Response::from_string(format!("400: {message}")).with_status_code(StatusCode(400)))
+fn serve_400(request: Request, message: &str, error: &str) -> io::Result<()> {
+    serve_error(request, message, StatusCode(400), true, error)
 }
 
 fn serve_bytes(request: Request, bytes: &[u8], content_type: &str) -> io::Result<()> {
@@ -24,40 +43,33 @@ fn serve_bytes(request: Request, bytes: &[u8], content_type: &str) -> io::Result
     request.respond(Response::from_data(bytes).with_header(content_type_header))
 }
 
-// TODO: the errors of serve_api_search should probably return JSON
-// 'Cause that's what expected from them.
-fn serve_api_search(model: Arc<Mutex<Model>>, mut request: Request) -> io::Result<()> {
+fn serve_api_search(model: Arc<Mutex<Model>>, mut request: Request, content_type_header: Header) -> io::Result<()> {
     let mut buf = Vec::new();
     if let Err(err) = request.as_reader().read_to_end(&mut buf) {
-        eprintln!("ERROR: could not read the body of the request: {err}");
-        return serve_500(request);
+        return serve_500(request, "Could not read the body of the request", &err.to_string());
     }
 
     let body = match str::from_utf8(&buf) {
         Ok(body) => body.chars().collect::<Vec<_>>(),
         Err(err) => {
-            eprintln!("ERROR: could not interpret body as UTF-8 string: {err}");
-            return serve_400(request, "Body must be a valid UTF-8 string");
+            return serve_400(request, "Body must be a valid UTF-8 string", &err.to_string());
         }
     };
 
     let model = model.lock().unwrap();
     let result = model.search_query(&body);
 
-    let json = match serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()) {
-        Ok(json) => json,
+    match serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()) {
+        Ok(json) => {
+            request.respond(Response::from_string(&json).with_header(content_type_header))
+        },
         Err(err) => {
-            eprintln!("ERROR: could not convert search results to JSON: {err}");
-            return serve_500(request)
+            serve_500(request, "Error happened serving api search", &err.to_string())
         }
-    };
-
-    let content_type_header = Header::from_bytes("Content-Type", "application/json")
-        .expect("That we didn't put any garbage in the headers");
-    request.respond(Response::from_string(&json).with_header(content_type_header))
+    }
 }
 
-fn serve_api_stats(model: Arc<Mutex<Model>>, request: Request) -> io::Result<()> {
+fn serve_api_stats(model: Arc<Mutex<Model>>, request: Request, content_type_header: Header) -> io::Result<()> {
     use serde::Serialize;
 
     #[derive(Default, Serialize)]
@@ -73,28 +85,28 @@ fn serve_api_stats(model: Arc<Mutex<Model>>, request: Request) -> io::Result<()>
         stats.terms_count = model.df.len();
     }
 
-    let json = match serde_json::to_string(&stats) {
-        Ok(json) => json,
+    match serde_json::to_string(&stats) {
+        Ok(json) => {
+            return request.respond(Response::from_string(&json).with_header(content_type_header));
+        }
         Err(err) => {
-            eprintln!("ERROR: could not convert stats results to JSON: {err}");
-            return serve_500(request)
+            return serve_500(request, "Error happened serving api stats", &err.to_string());
         }
     };
-
-    let content_type_header = Header::from_bytes("Content-Type", "application/json")
-        .expect("That we didn't put any garbage in the headers");
-    request.respond(Response::from_string(&json).with_header(content_type_header))
 }
 
 fn serve_request(model: Arc<Mutex<Model>>, request: Request) -> io::Result<()> {
+    let json_content_type = Header::from_bytes("Content-Type", "application/json")
+        .expect("That we didn't put any garbage in the headers");
+
     println!("received request! method: {:?}, url: {:?}", request.method(), request.url());
 
     match (request.method(), request.url()) {
         (Method::Post, "/api/search") => {
-            serve_api_search(model, request)
+            serve_api_search(model, request, json_content_type)
         }
         (Method::Get, "/api/stats") => {
-            serve_api_stats(model, request)
+            serve_api_stats(model, request, json_content_type)
         }
         (Method::Get, "/index.css") => {
             serve_bytes(request, include_bytes!("../public/index.css"), "text/css")
